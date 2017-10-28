0977ea396f724870bc1173106051e55b9a7a2fb0
Roger Welsh <rjhwelsh@protonmail.ch>
2017-10-29 09:24:13 +1300

    Added my current emacs configuration. A bit messy, but it is what it is.

---
+++ init.el
@@ -0,0 +1,45 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
+
+;; The following sequence of commands is required to bootstrap org-mode
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+(setq package-load-list '(all))
+
+(unless (package-installed-p 'org-plus-contrib)
+  (package-install 'org-plus-contrib))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(require 'org)
+;; (use-package org :ensure t :pin org )
+;; Org is now bootstrapped
+
+(org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 70 :width normal :foundry "PfEd" :family "Hermit"))))
+ '(org-todo ((t (:background "#dd2c00" :foreground "black" :weight bold)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(calendar-date-style (quote iso))
+ '(package-selected-packages (quote (org-edna use-package))))
