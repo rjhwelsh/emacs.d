dbeb00819b1570a45d8e1b1833f48b1a721095e3
Roger Welsh <rjhwelsh@gmail.com>
2018-08-08 13:02:19 +1200

    init.el: Customized default tag face.

--- init.el
+++ init.el
@@ -43,3 +43,15 @@
 
 ;; Main config
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-tag ((t (:background "gray6" :foreground "white smoke" :slant oblique :weight bold :height 0.8)))))
