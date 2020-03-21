58bce1c5a0c2bff7699b61b3133c36c6deae2fb8
Roger Welsh <rjhwelsh@gmail.com>
2020-03-21 17:46:37 +1300

    init: Adjust indent (of comment)

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-;(package-initialize)
+;; (package-initialize)
 
 ;; The following sequence of commands is required to bootstrap org-mode
 (require 'package)
